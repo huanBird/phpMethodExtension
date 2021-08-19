596149742c988fa62ef7315790e5d60f3bc2396b
huanbird <737558850@qq.com>
2021-08-19 14:18:26 +0800

    add new method backIcd

--- src/MethodExtension.php
+++ src/MethodExtension.php
@@ -14,4 +14,20 @@ class MethodExtension
     {
         return strlen(trim($str));
     }
+
+    /**
+     * 银行卡卡号掩码 默认前四位 后四位
+     * @param $bankCardNo
+     * @param int $fistNum
+     * @param int $backNum
+     * @return string
+     */
+    public function formatBankCardNo($bankCardNo,$fistNum = 4,$backNum = 4){
+        //截取银行卡号前4位
+        $prefix = substr($bankCardNo,0,$fistNum);
+        //截取银行卡号后4位
+        $suffix = substr($bankCardNo,'-'.$backNum,$backNum);
+        $maskBankCardNo = $prefix." **** **** **** ".$suffix;
+        return $maskBankCardNo;
+    }
 }
