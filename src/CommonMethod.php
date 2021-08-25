d5fe9dcf0b462ecbcd71f1323e868ecdcf1672f1
yuhou.wang <952945457@qq.com>
2021-08-25 10:22:42 +0800

    方法判断修正

--- src/CommonMethod.php
+++ src/CommonMethod.php
@@ -155,20 +155,20 @@ if (!function_exists('priceFormat')){
         if($show_yuan == false) return sprintf("%s", $price);
         else return sprintf("￥%s元", $price);
     }
+}
+
+if (!function_exists('uuid')){
     // 简易UUID生成方法
-    function uuid() {
+    function uuid(){
         $charid = md5(uniqid(mt_rand(), true));
         $hyphen = chr(45);
         $uuid = chr(123)
                .substr($charid, 0, 8).$hyphen
                .substr($charid, 8, 4).$hyphen
-               .substr($charid,12, 4).$hyphen
-               .substr($charid,16, 4).$hyphen
-               .substr($charid,20,12)
+               .substr($charid, 12, 4).$hyphen
+               .substr($charid, 16, 4).$hyphen
+               .substr($charid, 20, 12)
                .chr(125);
         return $uuid;
     }
 }
-
-
-
